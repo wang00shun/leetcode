76c107f057eef47b9d18c1d1fd48c8ba1b7ebffd
wang <1061147291@qq.com>
2022-06-05 22:27:01 +0800

    move array 45

---
+++ src/45.跳跃游戏-ii.rs
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=rust
+ *
+ * [45] 跳跃游戏 II
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        let len = nums.len();
+        if len <= 1 {
+            return 0;
+        }
+        let mut count = 0;
+        let mut index = 0;
+        while index < len {
+            count += 1;
+            let cur = nums[index] as usize;
+            if cur + index >= len - 1 {
+                break;
+            }
+            let mut max_Index = 0;
+            let mut max = 0;
+            for add in 1..=cur {
+                let next_index = index + add;
+                let next_num = nums[next_index] as usize;
+                let next_max = next_num + next_index;
+                if next_max >= max {
+                    max = next_max;
+                    max_Index = next_index;
+                }
+            }
+            index = max_Index;
+        }
+        return count;
+    }
+}
+// @lc code=end
